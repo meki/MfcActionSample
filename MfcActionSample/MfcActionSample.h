c25b5d9cf65c78fcf37446d2a320586584d62d8b
meki <sashidapc373443@gmail.com>
2022-11-03 02:54:12 +0900

    first commit

---
+++ MfcActionSample/MfcActionSample.h
@@ -0,0 +1,32 @@
+﻿
+// MfcActionSample.h : PROJECT_NAME アプリケーションのメイン ヘッダー ファイルです
+//
+
+#pragma once
+
+#ifndef __AFXWIN_H__
+	#error "PCH に対してこのファイルをインクルードする前に 'pch.h' をインクルードしてください"
+#endif
+
+#include "resource.h"		// メイン シンボル
+
+
+// CMfcActionSampleApp:
+// このクラスの実装については、MfcActionSample.cpp を参照してください
+//
+
+class CMfcActionSampleApp : public CWinApp
+{
+public:
+	CMfcActionSampleApp();
+
+// オーバーライド
+public:
+	virtual BOOL InitInstance();
+
+// 実装
+
+	DECLARE_MESSAGE_MAP()
+};
+
+extern CMfcActionSampleApp theApp;
---
+++ MfcActionSample/MfcActionSampleDlg.h
@@ -0,0 +1,34 @@
+﻿
+// MfcActionSampleDlg.h : ヘッダー ファイル
+//
+
+#pragma once
+
+
+// CMfcActionSampleDlg ダイアログ
+class CMfcActionSampleDlg : public CDialogEx
+{
+// コンストラクション
+public:
+	CMfcActionSampleDlg(CWnd* pParent = nullptr);	// 標準コンストラクター
+
+// ダイアログ データ
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_MFCACTIONSAMPLE_DIALOG };
+#endif
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV サポート
+
+
+// 実装
+protected:
+	HICON m_hIcon;
+
+	// 生成された、メッセージ割り当て関数
+	virtual BOOL OnInitDialog();
+	afx_msg void OnSysCommand(UINT nID, LPARAM lParam);
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	DECLARE_MESSAGE_MAP()
+};
